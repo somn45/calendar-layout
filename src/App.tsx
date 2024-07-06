9e88fe31c68862d16bafba34f833c10f5703581d
somn45 <somnsomn45@gmail.com>
2024-07-06 22:58:28 +0900

    현재 달이 며칠인지, 오늘이 무슨 요일인지 구하는 함수 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const day1 = new Date();
+    const calMonthDay = () => {
+      // 한 달에 며칠이 포함되어 있는가
+      const dates = new Date(day1.getFullYear(), day1.getMonth() + 1, 0).getDate();
+      console.log(dates);
+
+      // 무슨 요일인가?
+      const today = new Date(Date.now()).getDay();
+      console.log('무슨 요일?', today);
+    }
+    calMonthDay();
+  }, []);
   return (
     <div>
     </div>
