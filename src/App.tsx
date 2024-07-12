9a15e3b6a8890061d750a25b99ae6c8f5230064a
somn45 <somnsomn45@gmail.com>
2024-07-12 22:21:17 +0900

    달력 생성 함수 개선 : date-fns 모듈을 사용

--- src/App.tsx
+++ src/App.tsx
@@ -4,68 +4,76 @@ import styled from 'styled-components';
 import { getDaysInMonth } from 'date-fns';
 import Week from './Week';
 
-const MonthCalendar = styled.ul`
-  display: flex;
-  flex-direction: column;
+const CalendarLayout = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(7, 40px);
   list-style-type: none;
 `;
 
+const CalendarDateItem = styled.li<{$iscurrentdate: boolean}>`
+  color: ${props => props.$iscurrentdate ? '#000' : '#555'};
+`;
+
+interface CalendarDate {
+  key: number;
+  date: number;
+  isCurrentDate: boolean;
+}
+
+
+
 function App() {
-  const [calendar, setCalendar] = useState<number[][] | null>(null)
+  const [currentDay, setCurrentDay] = useState(new Date());
+  const [calendar, setCalendar] = useState<CalendarDate[] | null>(null)
   useEffect(() => {
     createMonthDays();
   }, []);
 
   const createMonthDays = () => {
-    const now = new Date();
-    const nowMonthFirstDate = new Date(now.getFullYear(), now.getMonth(), 1);
+    // 현재 달의 첫째 날 (ex. 현재 7월일 때 => 7월 1일)
+    const nowMonthFirstDate = new Date(currentDay.getFullYear(), currentDay.getMonth(), 1);
     // 무슨 요일이지?
     const day = nowMonthFirstDate.getDay();
-    // 몇일이지?
-    let currentDate = nowMonthFirstDate.getDate();
+
+    // 현재 달의 요일은 얼마나 있는가? (ex. 현재 7월일 때 => 31) 
     const totalDateCurrentMonth = getDaysInMonth(new Date(nowMonthFirstDate.getFullYear(), nowMonthFirstDate.getMonth()));
 
-    // 현재 달의 달력
-    let calendar: number[][] = [];
-    while(currentDate <= totalDateCurrentMonth) {
-      const week = createWeekCalendar(currentDate, day);
-      calendar.push(week);
-      currentDate += 7;
-    }
-    calendar = calendar.map((week, index) => {
-      return index === 0 ? findLastMonthDate(calendar[index]) : index === calendar.length - 1 ? findNextMonthDate(calendar[index]) : week;
+    const calendar = Array.from({length: 35}, (v, i) => i - day + 1).map(date => {
+      if(date < 1) {
+        const lastMonthFirstDate = new Date(currentDay.getFullYear(), currentDay.getMonth() - 1, 1);
+        const totalDateLastMonth = getDaysInMonth(new Date(lastMonthFirstDate));
+        return {
+          key: date,
+          date: date + totalDateLastMonth,
+          isCurrentDate: false,
+        };
+      }
+      else if(date > totalDateCurrentMonth) {
+        const nextMonthFirstDate = new Date(currentDay.getFullYear(), currentDay.getMonth() + 1, 1);
+        const totalDateNextMonth = getDaysInMonth(new Date(nextMonthFirstDate));
+        return {
+          key: date,
+          date: date - totalDateNextMonth,
+          isCurrentDate: false,
+        };
+      }
+      return {
+        key: date,
+        date,
+        isCurrentDate: true,
+      };
     })
     setCalendar(calendar);
-  }
-
-  const findNextMonthDate = (week: number[]) => {
-    const totalDateCurrentMonth = getDaysInMonth(new Date(new Date().getFullYear(), new Date().getMonth()));
-    const includeNextMonthDate = week.map(date => {
-      return date > totalDateCurrentMonth ? date - totalDateCurrentMonth : date;
-    })
-    return includeNextMonthDate;
-  }
-
-  const findLastMonthDate = (week: number[]) => {
-    const totalDateLastMonth = getDaysInMonth(new Date(new Date().getFullYear(), new Date().getMonth() - 1));
-    const includeLastMonthDate = week.map((date) => {
-      return date < 1 ? date + totalDateLastMonth : date;
-    });
-    return includeLastMonthDate;
-  }
-
-  const createWeekCalendar = (startDate: number, day: number) => {
-    return Array.from(new Array(7), (startDate, i) => (i + 1)).map(date => date + startDate - 1 - day);
   }
 
   return (
     <section>
       <h1>캘린더</h1>
-      <MonthCalendar>
-      {calendar?.map(week => (
-        <Week key={week[0]} week={week} />
-      ))}
-      </MonthCalendar>
+      <CalendarLayout>
+        {calendar?.map(date => (
+          <CalendarDateItem key={date.key} $iscurrentdate={date.isCurrentDate}>{date.date}</CalendarDateItem>
+        ))}
+      </CalendarLayout>
     </section>
   );
 }
